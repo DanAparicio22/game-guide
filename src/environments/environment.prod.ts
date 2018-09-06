b82b24807130c12c0737ae1946b51415a83723c9
DanAparicio22 <daparicio52@gmail.com>
2018-09-06 19:25:05 -0400

    TRE-1 (Add configuration in angular 6 for firestore) | Add variables in environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDLPDR9JT1r400_JoF_BL_BkmUpJEI9fnQ',
+    authDomain: 'game-guides-e33b2.firebaseapp.com',
+    databaseURL: 'https://game-guides-e33b2.firebaseio.com',
+    projectId: 'game-guides-e33b2',
+    storageBucket: 'game-guides-e33b2.appspot.com',
+    messagingSenderId: '218227664902'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,15 +1,11 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDLPDR9JT1r400_JoF_BL_BkmUpJEI9fnQ',
+    authDomain: 'game-guides-e33b2.firebaseapp.com',
+    databaseURL: 'https://game-guides-e33b2.firebaseio.com',
+    projectId: 'game-guides-e33b2',
+    storageBucket: 'game-guides-e33b2.appspot.com',
+    messagingSenderId: '218227664902'
+  }
 };
-
-/*
- * In development mode, to ignore zone related error stack frames such as
- * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
- * import the following file, but please comment it out in production mode
- * because it will have performance impact when throw error
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
